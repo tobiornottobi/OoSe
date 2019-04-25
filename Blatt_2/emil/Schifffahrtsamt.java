8a394a0b636336f6ac41c2e2842f42df44477bce
ole-thoeb <43213977+ole-thoeb@users.noreply.github.com>
2019-04-25 10:19:51 +0200

    quick fix

--- Blatt_2/emil/Schifffahrtsamt.java
+++ Blatt_2/emil/Schifffahrtsamt.java
@@ -6,6 +6,7 @@ public class Schifffahrtsamt {
 	public void regestriereSchiff(Schiff schiff) {
 		if (numberOfRegistertShips < schiffe.length) {
 			schiffe[numberOfRegistertShips] = schiff;
+			numberOfRegistertShips++;
 		}
 	}
 
