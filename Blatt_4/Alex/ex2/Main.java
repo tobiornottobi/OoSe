8ec5b4fbf7521f604f22be803a88045eed946327
Naumal <49651669+Naumal@users.noreply.github.com>
2019-05-08 14:48:56 +0200

    Add files via upload

---
+++ Blatt_4/Alex/ex2/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main (String[] args) {
+        Student student = new Student();
+        Cauliflower essen1= new Cauliflower();
+        student.eat(essen1); // I dont like Blumenkohl an Goronzolasoße, denn durch new Cauliflower wird die Methode dieser Klasse ausgeführt, wie in Student definiert
+        Spaghetti essen2 = new Spaghetti();
+        student.eat(essen2);// I like Spaghetti Bologneser Art, die student.eat() Funktion wird der ersten mit dem Scope "Food food" zugeordnet und die Funktion aus Spaghetti
+                            // überschreibt die von Cauliflower, da essen2 ein Objekt vom statischen und dynamischen Typ Spaghetti ist
+        Food essen = new Cauliflower(); 
+        student.eat(essen); // I like Blumenkohl an Gorgonzolasoße, essen ist vom statischen Typ Food, somit wird die erste der beiden student.eat() Funktionen aufgerufen
+                            // "getmeal()" bezieht sich dann wieder auf Cauliflower, da dessen Konstruktor verwendet wurde, um essen zu initialisieren (dynamischer Typ, dynamisches Binden der getMeal() Methode)
+    }
+}
